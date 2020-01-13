2b6c931c79dd4fba7e862943842006322442ce2c
Jasin <Jusufjasin@gmail.com>
2020-01-13 09:56:02 +0100

    Added files

---
+++ Formular.php
@@ -0,0 +1,7 @@
+<?php
+    echo '<u> Deine Angaben: </u> <br>';
+    echo 'Dein Name: ' .$_POST['vorname'] .'<br>';
+    echo 'Dein Nachname: ' .$_POST['nachname'] .'<br>';
+    echo 'Deine Rückmeldung: ' .$_POST['nachricht'] .'<br>';
+    echo 'Deine E-Mail: ' .$_POST['email'];
+    ?>
